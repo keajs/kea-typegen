e3006c2d82733495da8b4df8325773a027927444
Marius Andra <marius.andra@gmail.com>
2020-07-07 22:45:29 +0200

    create a simple command line wrapper

--- bin/index.ts
+++ bin/index.ts
@@ -1,10 +1,37 @@
 // https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API#using-the-type-checker
 import * as ts from 'typescript'
 import { visitProgram } from '../src/visit/visit'
+import * as yargs from 'yargs'
 
-const program = ts.createProgram(['./bin/input/logic.ts'], {
-    target: ts.ScriptTarget.ES5,
-    module: ts.ModuleKind.CommonJS,
-})
+const parser = yargs
+    .usage('Use one of:\n - kea-typegen -f logic.ts\n - kea-typegen -c tsconfig.json')
+    .option('f', { alias: 'file', describe: 'Logic file', type: 'string' })
+    .option('c', { alias: 'config', describe: 'Path to tsconfig.json', type: 'string' })
 
-visitProgram(program)
+const options = parser.argv
+
+let program
+
+if (options.file) {
+    program = ts.createProgram([options.file as string], {
+        target: ts.ScriptTarget.ES5,
+        module: ts.ModuleKind.CommonJS,
+    })
+} else {
+    const configFileName = options.config || ts.findConfigFile('./', ts.sys.fileExists, 'tsconfig.json')
+
+    if (configFileName) {
+        console.log(`Using Config: ${configFileName}`)
+        const configFile = ts.readConfigFile(configFileName as string, ts.sys.readFile)
+        const compilerOptions = ts.parseJsonConfigFileContent(configFile.config, ts.sys, './')
+
+        const host = ts.createCompilerHost(compilerOptions.options)
+        program = ts.createProgram([], compilerOptions.options, host)
+    }
+}
+
+if (program) {
+    const parsedLogics = visitProgram(program)
+} else {
+    parser.showHelp()
+}
