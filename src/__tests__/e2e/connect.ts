ca5cfe7b625bef188c87ac916af90bc31645e351
Marius Andra <marius.andra@gmail.com>
2020-07-15 23:23:17 +0200

    connect with test broken

---
+++ src/__tests__/e2e/connect.ts
@@ -0,0 +1,47 @@
+import {logicSourceToLogicType} from "../../utils";
+
+test('connect from another logic with a given type', () => {
+    const logicSource = `
+        import { kea } from 'kea'
+        
+        export interface otherLogicType {
+            actionCreators: {
+                updateName: (name: string) => ({
+                    type: \\\\"update name (logic)\\\\";
+                    payload: { name: string; };
+                });
+                updateOtherName: (otherName: string) => ({
+                    type: \\\\"update other name (logic)\\\\";
+                    payload: { otherName: string; };
+                });
+            };
+            actions: {
+                updateName: (name: string) => ({
+                    type: \\\\"update name (logic)\\\\";
+                    payload: { name: string; };
+                });
+                updateOtherName: (otherName: string) => ({
+                    type: \\\\"update other name (logic)\\\\";
+                    payload: { otherName: string; };
+                });
+            };
+            reducer: (state: any, action: () => any, fullState: any) => {};
+            reducers: {};
+            selector: (state: any) => {};
+            selectors: {};
+            values: {};
+        }
+        
+        const logic = kea({
+            connect: {
+                actions: [
+                    otherLogic as otherLogicType, [
+                        'updateName',  
+                        'updateOtherName',  
+                    ]
+                ]
+            }
+        })
+    `
+    expect(logicSourceToLogicType(logicSource)).toMatchSnapshot()
+})
