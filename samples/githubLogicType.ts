3a61a54faf1e022ecc29b342942f1d6541c7a7f5
Marius Andra <marius.andra@gmail.com>
2020-07-28 22:54:24 +0200

    run logic through prettier

--- samples/githubLogicType.ts
+++ samples/githubLogicType.ts
@@ -1,89 +1,107 @@
 // Auto-generated with kea-typegen. DO NOT EDIT!
 
 export interface githubLogicType<Repository> {
-    key: any;
+    key: any
     actionCreators: {
-        setUsername: (username: string) => ({
-            type: "set username (samples.githubLogic)";
-            payload: { username: string; };
-        });
-        setRepositories: (repositories: Repository[]) => ({
-            type: "set repositories (samples.githubLogic)";
-            payload: { repositories: Repository[]; };
-        });
-        setFetchError: (error: string) => ({
-            type: "set fetch error (samples.githubLogic)";
-            payload: { error: string; };
-        });
-    };
+        setUsername: (
+            username: string,
+        ) => {
+            type: 'set username (samples.githubLogic)'
+            payload: { username: string }
+        }
+        setRepositories: (
+            repositories: Repository[],
+        ) => {
+            type: 'set repositories (samples.githubLogic)'
+            payload: { repositories: Repository[] }
+        }
+        setFetchError: (
+            error: string,
+        ) => {
+            type: 'set fetch error (samples.githubLogic)'
+            payload: { error: string }
+        }
+    }
     actionKeys: {
-        "set username (samples.githubLogic)": "setUsername";
-        "set repositories (samples.githubLogic)": "setRepositories";
-        "set fetch error (samples.githubLogic)": "setFetchError";
-    };
+        'set username (samples.githubLogic)': 'setUsername'
+        'set repositories (samples.githubLogic)': 'setRepositories'
+        'set fetch error (samples.githubLogic)': 'setFetchError'
+    }
     actionTypes: {
-        setUsername: "set username (samples.githubLogic)";
-        setRepositories: "set repositories (samples.githubLogic)";
-        setFetchError: "set fetch error (samples.githubLogic)";
-    };
+        setUsername: 'set username (samples.githubLogic)'
+        setRepositories: 'set repositories (samples.githubLogic)'
+        setFetchError: 'set fetch error (samples.githubLogic)'
+    }
     actions: {
-        setUsername: (username: string) => ({
-            type: "set username (samples.githubLogic)";
-            payload: { username: string; };
-        });
-        setRepositories: (repositories: Repository[]) => ({
-            type: "set repositories (samples.githubLogic)";
-            payload: { repositories: Repository[]; };
-        });
-        setFetchError: (error: string) => ({
-            type: "set fetch error (samples.githubLogic)";
-            payload: { error: string; };
-        });
-    };
-    cache: Record<string, any>;
-    connections: any;
-    constants: any;
-    defaults: any;
-    events: any;
-    path: ["samples", "githubLogic"];
-    pathString: "samples.githubLogic";
-    propTypes: any;
-    props: Record<string, any>;
-    reducer: (state: any, action: () => any, fullState: any) => {
-        username: string;
-        repositories: Repository[];
-        isLoading: boolean;
-        error: string | null;
-    };
-    reducerOptions: any;
+        setUsername: (
+            username: string,
+        ) => {
+            type: 'set username (samples.githubLogic)'
+            payload: { username: string }
+        }
+        setRepositories: (
+            repositories: Repository[],
+        ) => {
+            type: 'set repositories (samples.githubLogic)'
+            payload: { repositories: Repository[] }
+        }
+        setFetchError: (
+            error: string,
+        ) => {
+            type: 'set fetch error (samples.githubLogic)'
+            payload: { error: string }
+        }
+    }
+    cache: Record<string, any>
+    connections: any
+    constants: any
+    defaults: any
+    events: any
+    path: ['samples', 'githubLogic']
+    pathString: 'samples.githubLogic'
+    propTypes: any
+    props: Record<string, any>
+    reducer: (
+        state: any,
+        action: () => any,
+        fullState: any,
+    ) => {
+        username: string
+        repositories: Repository[]
+        isLoading: boolean
+        error: string | null
+    }
+    reducerOptions: any
     reducers: {
-        username: (state: string, action: any, fullState: any) => string;
-        repositories: (state: Repository[], action: any, fullState: any) => Repository[];
-        isLoading: (state: boolean, action: any, fullState: any) => boolean;
-        error: (state: string | null, action: any, fullState: any) => string | null;
-    };
-    selector: (state: any) => {
-        username: string;
-        repositories: Repository[];
-        isLoading: boolean;
-        error: string | null;
-    };
+        username: (state: string, action: any, fullState: any) => string
+        repositories: (state: Repository[], action: any, fullState: any) => Repository[]
+        isLoading: (state: boolean, action: any, fullState: any) => boolean
+        error: (state: string | null, action: any, fullState: any) => string | null
+    }
+    selector: (
+        state: any,
+    ) => {
+        username: string
+        repositories: Repository[]
+        isLoading: boolean
+        error: string | null
+    }
     selectors: {
-        username: (state: any, props: any) => string;
-        repositories: (state: any, props: any) => Repository[];
-        isLoading: (state: any, props: any) => boolean;
-        error: (state: any, props: any) => string | null;
-        sortedRepositories: (state: any, props: any) => Repository[];
-    };
+        username: (state: any, props: any) => string
+        repositories: (state: any, props: any) => Repository[]
+        isLoading: (state: any, props: any) => boolean
+        error: (state: any, props: any) => string | null
+        sortedRepositories: (state: any, props: any) => Repository[]
+    }
     values: {
-        username: string;
-        repositories: Repository[];
-        isLoading: boolean;
-        error: string | null;
-        sortedRepositories: Repository[];
-    };
-    _isKea: true;
+        username: string
+        repositories: Repository[]
+        isLoading: boolean
+        error: string | null
+        sortedRepositories: Repository[]
+    }
+    _isKea: true
     __keaTypeGenInternalSelectorTypes: {
-        sortedRepositories: (arg1: Repository[]) => Repository[];
-    };
+        sortedRepositories: (arg1: Repository[]) => Repository[]
+    }
 }
--- samples/logicType.ts
+++ samples/logicType.ts
@@ -1,138 +1,166 @@
 // Auto-generated with kea-typegen. DO NOT EDIT!
 
 export interface logicType<Session> {
-    key: any;
+    key: any
     actionCreators: {
-        updateName: (name: string) => ({
-            type: "update name (samples.logic)";
-            payload: { name: string; };
-        });
-        updateNumber: (number: number) => ({
-            type: "update number (samples.logic)";
-            payload: { number: number; };
-        });
-        loadSessions: (selectedDate: string) => ({
-            type: "load sessions (samples.logic)";
-            payload: string;
-        });
-        loadSessionsSuccess: (sessions: Session[]) => ({
-            type: "load sessions success (samples.logic)";
+        updateName: (
+            name: string,
+        ) => {
+            type: 'update name (samples.logic)'
+            payload: { name: string }
+        }
+        updateNumber: (
+            number: number,
+        ) => {
+            type: 'update number (samples.logic)'
+            payload: { number: number }
+        }
+        loadSessions: (
+            selectedDate: string,
+        ) => {
+            type: 'load sessions (samples.logic)'
+            payload: string
+        }
+        loadSessionsSuccess: (
+            sessions: Session[],
+        ) => {
+            type: 'load sessions success (samples.logic)'
             payload: {
-                sessions: Session[];
-            };
-        });
-        loadSessionsFailure: (error: string) => ({
-            type: "load sessions failure (samples.logic)";
+                sessions: Session[]
+            }
+        }
+        loadSessionsFailure: (
+            error: string,
+        ) => {
+            type: 'load sessions failure (samples.logic)'
             payload: {
-                error: string;
-            };
-        });
-    };
+                error: string
+            }
+        }
+    }
     actionKeys: {
-        "update name (samples.logic)": "updateName";
-        "update number (samples.logic)": "updateNumber";
-        "load sessions (samples.logic)": "loadSessions";
-        "load sessions success (samples.logic)": "loadSessionsSuccess";
-        "load sessions failure (samples.logic)": "loadSessionsFailure";
-    };
+        'update name (samples.logic)': 'updateName'
+        'update number (samples.logic)': 'updateNumber'
+        'load sessions (samples.logic)': 'loadSessions'
+        'load sessions success (samples.logic)': 'loadSessionsSuccess'
+        'load sessions failure (samples.logic)': 'loadSessionsFailure'
+    }
     actionTypes: {
-        updateName: "update name (samples.logic)";
-        updateNumber: "update number (samples.logic)";
-        loadSessions: "load sessions (samples.logic)";
-        loadSessionsSuccess: "load sessions success (samples.logic)";
-        loadSessionsFailure: "load sessions failure (samples.logic)";
-    };
+        updateName: 'update name (samples.logic)'
+        updateNumber: 'update number (samples.logic)'
+        loadSessions: 'load sessions (samples.logic)'
+        loadSessionsSuccess: 'load sessions success (samples.logic)'
+        loadSessionsFailure: 'load sessions failure (samples.logic)'
+    }
     actions: {
-        updateName: (name: string) => ({
-            type: "update name (samples.logic)";
-            payload: { name: string; };
-        });
-        updateNumber: (number: number) => ({
-            type: "update number (samples.logic)";
-            payload: { number: number; };
-        });
-        loadSessions: (selectedDate: string) => ({
-            type: "load sessions (samples.logic)";
-            payload: string;
-        });
-        loadSessionsSuccess: (sessions: Session[]) => ({
-            type: "load sessions success (samples.logic)";
+        updateName: (
+            name: string,
+        ) => {
+            type: 'update name (samples.logic)'
+            payload: { name: string }
+        }
+        updateNumber: (
+            number: number,
+        ) => {
+            type: 'update number (samples.logic)'
+            payload: { number: number }
+        }
+        loadSessions: (
+            selectedDate: string,
+        ) => {
+            type: 'load sessions (samples.logic)'
+            payload: string
+        }
+        loadSessionsSuccess: (
+            sessions: Session[],
+        ) => {
+            type: 'load sessions success (samples.logic)'
             payload: {
-                sessions: Session[];
-            };
-        });
-        loadSessionsFailure: (error: string) => ({
-            type: "load sessions failure (samples.logic)";
+                sessions: Session[]
+            }
+        }
+        loadSessionsFailure: (
+            error: string,
+        ) => {
+            type: 'load sessions failure (samples.logic)'
             payload: {
-                error: string;
-            };
-        });
-    };
-    cache: Record<string, any>;
-    connections: any;
-    constants: any;
-    defaults: any;
-    events: any;
-    path: ["samples", "logic"];
-    pathString: "samples.logic";
-    propTypes: any;
-    props: Record<string, any>;
-    reducer: (state: any, action: () => any, fullState: any) => {
-        name: string;
-        number: number;
-        otherNameNoDefault: any;
-        yetAnotherNameWithNullDefault: string | null;
-        sessions: Session[];
-        sessionsLoading: boolean;
-    };
-    reducerOptions: any;
+                error: string
+            }
+        }
+    }
+    cache: Record<string, any>
+    connections: any
+    constants: any
+    defaults: any
+    events: any
+    path: ['samples', 'logic']
+    pathString: 'samples.logic'
+    propTypes: any
+    props: Record<string, any>
+    reducer: (
+        state: any,
+        action: () => any,
+        fullState: any,
+    ) => {
+        name: string
+        number: number
+        otherNameNoDefault: any
+        yetAnotherNameWithNullDefault: string | null
+        sessions: Session[]
+        sessionsLoading: boolean
+    }
+    reducerOptions: any
     reducers: {
-        name: (state: string, action: any, fullState: any) => string;
-        number: (state: number, action: any, fullState: any) => number;
-        otherNameNoDefault: (state: any, action: any, fullState: any) => any;
-        yetAnotherNameWithNullDefault: (state: string | null, action: any, fullState: any) => string | null;
-        sessions: (state: Session[], action: any, fullState: any) => Session[];
-        sessionsLoading: (state: boolean, action: any, fullState: any) => boolean;
-    };
-    selector: (state: any) => {
-        name: string;
-        number: number;
-        otherNameNoDefault: any;
-        yetAnotherNameWithNullDefault: string | null;
-        sessions: Session[];
-        sessionsLoading: boolean;
-    };
+        name: (state: string, action: any, fullState: any) => string
+        number: (state: number, action: any, fullState: any) => number
+        otherNameNoDefault: (state: any, action: any, fullState: any) => any
+        yetAnotherNameWithNullDefault: (state: string | null, action: any, fullState: any) => string | null
+        sessions: (state: Session[], action: any, fullState: any) => Session[]
+        sessionsLoading: (state: boolean, action: any, fullState: any) => boolean
+    }
+    selector: (
+        state: any,
+    ) => {
+        name: string
+        number: number
+        otherNameNoDefault: any
+        yetAnotherNameWithNullDefault: string | null
+        sessions: Session[]
+        sessionsLoading: boolean
+    }
     selectors: {
-        name: (state: any, props: any) => string;
-        number: (state: any, props: any) => number;
-        otherNameNoDefault: (state: any, props: any) => any;
-        yetAnotherNameWithNullDefault: (state: any, props: any) => string | null;
-        sessions: (state: any, props: any) => Session[];
-        sessionsLoading: (state: any, props: any) => boolean;
-        capitalizedName: (state: any, props: any) => string;
-        upperCaseName: (state: any, props: any) => string;
-    };
+        name: (state: any, props: any) => string
+        number: (state: any, props: any) => number
+        otherNameNoDefault: (state: any, props: any) => any
+        yetAnotherNameWithNullDefault: (state: any, props: any) => string | null
+        sessions: (state: any, props: any) => Session[]
+        sessionsLoading: (state: any, props: any) => boolean
+        capitalizedName: (state: any, props: any) => string
+        upperCaseName: (state: any, props: any) => string
+    }
     values: {
-        name: string;
-        number: number;
-        otherNameNoDefault: any;
-        yetAnotherNameWithNullDefault: string | null;
-        sessions: Session[];
-        sessionsLoading: boolean;
-        capitalizedName: string;
-        upperCaseName: string;
-    };
-    _isKea: true;
+        name: string
+        number: number
+        otherNameNoDefault: any
+        yetAnotherNameWithNullDefault: string | null
+        sessions: Session[]
+        sessionsLoading: boolean
+        capitalizedName: string
+        upperCaseName: string
+    }
+    _isKea: true
     __keaTypeGenInternalSelectorTypes: {
-        capitalizedName: (arg1: string, arg2: number) => string;
-        upperCaseName: (arg1: string) => string;
-    };
+        capitalizedName: (arg1: string, arg2: number) => string
+        upperCaseName: (arg1: string) => string
+    }
     __keaTypeGenInternalReducerActions: {
-        "set username (samples.githubLogic)": (username: string) => ({
-            type: "set username (samples.githubLogic)";
+        'set username (samples.githubLogic)': (
+            username: string,
+        ) => {
+            type: 'set username (samples.githubLogic)'
             payload: {
-                username: string;
-            };
-        });
-    };
+                username: string
+            }
+        }
+    }
 }
