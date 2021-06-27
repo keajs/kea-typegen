fbb3675d3d5a54c7dc65829b5b245b6b36e729f0
Marius Andra <marius.andra@gmail.com>
2021-06-27 23:32:13 +0200

    revert to detected types if none given

--- src/visit/visitActions.ts
+++ src/visit/visitActions.ts
@@ -2,27 +2,30 @@ import { ParsedLogic } from '../types'
 import * as ts from 'typescript'
 import { NodeBuilderFlags } from 'typescript'
 import { cloneNode } from '@wessberg/ts-clone-node'
-import { gatherImports } from '../utils'
+import { gatherImports} from '../utils'
 
 export function visitActions(type: ts.Type, inputProperty: ts.PropertyAssignment, parsedLogic: ParsedLogic) {
     const { checker } = parsedLogic
     const properties = checker.getPropertiesOfType(type)
 
     for (const property of properties) {
-        const name = property.getName()
         const type = checker.getTypeOfSymbolAtLocation(property, property.valueDeclaration!)
-        const typeNode = checker.typeToTypeNode(type, undefined, undefined)
+        const name = property.getName()
 
         let returnTypeNode
         let parameters
 
-        if (ts.isFunctionTypeNode(typeNode)) {
-            const signature = type.getCallSignatures()[0]
-            parameters = signature.getDeclaration().parameters.map((param) => {
+        if (!ts.isPropertyAssignment(property.valueDeclaration)) {
+            continue
+        }
+        const { initializer } = property.valueDeclaration
+
+        if (ts.isFunctionLike(initializer)) {
+            // action is a function action: () => ({ ... })
+            parameters = initializer.parameters.map((param) => {
                 if (param.type) {
                     gatherImports(param.type, checker, parsedLogic)
                 }
-
                 return ts.createParameter(
                     undefined,
                     undefined,
@@ -34,18 +37,30 @@ export function visitActions(type: ts.Type, inputProperty: ts.PropertyAssignment
                 )
             })
 
-            const sigReturnType = signature.getReturnType()
-            const sigReturnTypeNode = checker.typeToTypeNode(sigReturnType, undefined, NodeBuilderFlags.NoTruncation)
+            // first try the specified type (action: (): Type => {...})
+            returnTypeNode = initializer.type
 
-            gatherImports(sigReturnTypeNode, checker, parsedLogic)
-            returnTypeNode = cloneNode(sigReturnTypeNode)
+            // if not found, use the TS compiler to detect it
+            if (!returnTypeNode) {
+                const signature = type.getCallSignatures()[0]
+                const sigReturnType = signature.getReturnType()
+                const sigReturnTypeNode = checker.typeToTypeNode(
+                    sigReturnType,
+                    undefined,
+                    NodeBuilderFlags.NoTruncation,
+                )
+                returnTypeNode = cloneNode(sigReturnTypeNode)
+            }
+
+            gatherImports(returnTypeNode, checker, parsedLogic)
         } else {
+            // action is a value (action: true)
+            const typeNode = checker.typeToTypeNode(type, undefined, undefined)
             gatherImports(typeNode, checker, parsedLogic)
             returnTypeNode = ts.createTypeLiteralNode([
                 ts.createPropertySignature(undefined, ts.createIdentifier('value'), undefined, typeNode, undefined),
             ])
         }
-
         parsedLogic.actions.push({ name, parameters, returnTypeNode })
     }
 }
